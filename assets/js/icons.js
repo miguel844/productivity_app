1d3914e552d9bceccf14d8cc23b325ac9b3da599
miguel844 <miguel844@gmail.com>
2026-07-17 00:32:48 +0200

    Fixed color dark mode

--- assets/js/icons.js
+++
@@ -1,3 +0,0 @@
-import { createIcons, icons } from "lucide";
-
-createIcons({ icons });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,21 @@
 // Exportemos los iconos desde el cdn de lucide
 lucide.createIcons();
+
+// Boton modo oscuro
+const darkMode = document.getElementById("darkMode");
+const logo = document.getElementById("logoMenu");
+const icon = document.getElementById("themeIcon");
+
+darkMode.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        logo.src = "/assets/icons/logo_M_hex_bgB.svg";
+        darkMode.innerHTML = '<i data-lucide="sun"></i>';
+    } else {
+        logo.src = "/assets/icons/logo_M_hex_bgW.svg";
+        darkMode.innerHTML = '<i data-lucide="moon"></i>';
+    }
+    lucide.createIcons();
+});
