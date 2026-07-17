0cd53f40d8ff49dd720641e2514c9701e46f11d8
miguel844 <miguel844@gmail.com>
2026-07-17 21:58:42 +0200

    Refactoring JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,60 @@
-// Exportemos los iconos desde el cdn de lucide
-lucide.createIcons();
+// INICIALIZACION ******************************************************************
+init();
+function init() {
+
+    lucide.createIcons(); // Exportamos los iconos desde el cdn de lucide
+    initDarkMode(); // Inicializamos el modo oscuro
+    const date = new Date(); // Inicializamos fecha
+    showGreeting(date); // Mostramos saludo
+    showDate(date); // Mostramos fecha
+}
+// FIN DE LA INICIALIZACION ********************************************************
 
 // Boton modo oscuro
-const darkMode = document.getElementById("darkMode");
-const logo = document.getElementById("logoMenu");
-const icon = document.getElementById("themeIcon");
+function initDarkMode() {
 
-darkMode.addEventListener("click", function () {
+    const darkMode = document.getElementById("darkMode");
+    const logo = document.getElementById("logoMenu");
 
-    document.body.classList.toggle("dark");
+    darkMode.addEventListener("click", function () {
 
-    if (document.body.classList.contains("dark")) {
-        logo.src = "/assets/icons/logo_M_hex_bgB.svg";
-        darkMode.innerHTML = '<i data-lucide="sun"></i>';
-    } else {
-        logo.src = "/assets/icons/logo_M_hex_bgW.svg";
-        darkMode.innerHTML = '<i data-lucide="moon"></i>';
-    }
-    lucide.createIcons();
-});
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+            logo.src = "/assets/icons/logo_M_hex_bgB.svg";
+            darkMode.innerHTML = '<i data-lucide="sun"></i>';
+        } else {
+            logo.src = "/assets/icons/logo_M_hex_bgW.svg";
+            darkMode.innerHTML = '<i data-lucide="moon"></i>';
+        }
+        lucide.createIcons();
+    });
+}
 
 // Fecha
-const currentDate = document.getElementById("currentDate");
+function showDate(date) {
 
-const date = new Date();
-const formatEs = new Intl.DateTimeFormat('es-ES', {
-    dateStyle: 'full'
-}).format(date);
+    const currentDate = document.getElementById("currentDate");
 
-currentDate.textContent = "Hoy es " + formatEs;
+    const formatEs = new Intl.DateTimeFormat('es-ES', {
+        dateStyle: 'full'
+    }).format(date);
+
+    currentDate.textContent = "Hoy es " + formatEs;
+}
 
 // Buenos dias, buenas tardes o buenas noches
-const currentHours = date.getHours();
-const goodDay = document.getElementById("goodDay");
-const nameUser = "Miguel Ángel";
+function showGreeting(date) {
 
-if (currentHours >= 6 && currentHours < 12) {
-    goodDay.textContent = "Buenos dias, " + nameUser + "!";
-} else if (currentHours >= 12 && currentHours < 21) {
-    goodDay.textContent = "Buenas tardes, " + nameUser + "!";
-} else {
-    goodDay.textContent = "Buenas noches, " + nameUser + "!";
+    const currentHours = date.getHours();
+    const goodDay = document.getElementById("goodDay");
+    const nameUser = "Miguel Ángel";
+
+    if (currentHours >= 6 && currentHours < 12) {
+        goodDay.textContent = `Buenos días, ${nameUser}!`;
+    } else if (currentHours >= 12 && currentHours < 21) {
+        goodDay.textContent = `Buenas tardes, ${nameUser}!`;
+    } else {
+        goodDay.textContent = `Buenas noches, ${nameUser}!`;
+    }
 }
-console.log(date.getHours());
